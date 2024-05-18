d073e7456e27948acd55772495daf921cc9d5c46
Joshua Li <JoshuaJLi@outlook.com>
2024-05-18 12:45:10 -0700

    Initial backend push.

---
+++ server/src/index.ts
@@ -0,0 +1,31 @@
+// src/index.js
+import express, { Express, Request, Response } from "express";
+import { createServer } from 'node:http';
+import dotenv from "dotenv";
+import cors from "cors";
+import { Server, Socket } from "socket.io"
+
+dotenv.config();
+const app: Express = express();
+
+const port = process.env.PORT || 8080;
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }}
+);
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get(`/`, (req: Request, res: Response) => {
+  res.send("It's working!");
+});
+
+
+server.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
