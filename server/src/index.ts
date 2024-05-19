699378c4648bdad791be7125e97f6bc076e4a855
elc10 <ethan_chou@sfu.ca>
2024-05-18 22:52:41 -0700

    get yesterday and tomorrow's dates for api calls

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,6 +13,27 @@ var gameList: Game[] = []
 dotenv.config();
 const app: Express = express();
 
+const todayRaw = new Date();
+const dd = String(todayRaw.getDate()).padStart(2, '0');
+const mm = String(todayRaw.getMonth() + 1).padStart(2, '0'); //January is 0!
+const yyyy = todayRaw.getFullYear();
+
+const today = String(yyyy + '-' + mm + '-' + dd);
+
+const yesterdayRaw = new Date(todayRaw);
+yesterdayRaw.setDate(yesterdayRaw.getDate() - 1);
+const ydd = String(yesterdayRaw.getDate()).padStart(2, '0');
+const ymm = String(yesterdayRaw.getMonth() + 1).padStart(2, '0'); //January is 0!
+const yyyyy = yesterdayRaw.getFullYear();
+const yesterday = String(yyyyy + '-' + ymm + '-' + ydd);
+
+const tomorrowRaw = new Date();
+const tdd = String(tomorrowRaw.getDate() + 1).padStart(2, '0');
+const tmm = String(tomorrowRaw.getMonth() + 1).padStart(2, '0'); //January is 0!
+const tyyyy = tomorrowRaw.getFullYear();
+const tomorrow = String(tyyyy + '-' + tmm + '-' + tdd);
+
+
 const port = process.env.PORT || 8080;
 const server = createServer(app);
 const io = new Server(server, {
@@ -54,6 +75,23 @@ axios
     console.log(gameList)
   });
 
+  const fetchNextDaysData = async () => {
+    try {
+      const response = await axios.get("https://api.github.com/users/mapbox");
+      console.log(response.data);
+      console.log(response.status);
+      console.log(response.statusText);
+      console.log(response.headers);
+      console.log(response.config);
+    } catch (error) {
+      // Handle error
+      console.error(error);
+    }
+  };
+
 server.listen(port, () => {
+  console.log(today);
+  console.log(yesterday);
+  console.log(tomorrow);
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
