40daefc2e5db59a292762ce473a84aa2c09e3521
elc10 <ethan_chou@sfu.ca>
2024-05-19 01:16:42 -0700

    moved functions from index to hockey helper + debug

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,12 +6,16 @@ import cors from "cors";
 import { Server, Socket } from "socket.io"
 import { userController } from "./controllers/user.controller";
 import { Game, result } from "./models/Game";
+import { hockeyHelper } from "./helpers/hockey.helper";
 const axios = require('axios').default;
 
 var gameList: Game[] = []
 
 dotenv.config();
 const app: Express = express();
+
+//TODO: get list of teams from frontend and ensure it is a string array
+var teams: string[] = ["EDM", "VAN"];
 
 const todayRaw = new Date();
 const dd = String(todayRaw.getDate()).padStart(2, '0');
@@ -33,6 +37,21 @@ const tmm = String(tomorrowRaw.getMonth() + 1).padStart(2, '0'); //January is 0!
 const tyyyy = tomorrowRaw.getFullYear();
 const tomorrow = String(tyyyy + '-' + tmm + '-' + tdd);
 
+// template code used to test hockeyHelper class
+
+// let testResult = hockeyHelper.getGames(teams);
+// testResult.then(testResult => {
+//   testResult.forEach(game => {
+//     console.log(game.gameTime + " - " + game.name);
+//   }) 
+// })
+
+// let todayScoreTestResult = hockeyHelper.getTomorrowsResults();
+// todayScoreTestResult.then(todayScoreTestResult => {
+//   todayScoreTestResult.forEach(game => {
+//     console.log(game.gameTime + " - " + game.name);
+//   }) 
+// })
 
 const port = process.env.PORT || 8080;
 const server = createServer(app);
@@ -53,78 +72,9 @@ app.get(`/`, (req: Request, res: Response) => {
 
 app.get('/user', userController);
 
-axios
-  .get("https://api-web.nhle.com/v1/score/now")
-  .then(function (response : any) {
-    //console.log(response.data.games);
-    response.data.games.forEach((element : any) => {
-      //console.log(element);
-      var newGame = new Game(
-        element.id, 
-        `${element.awayTeam.name.default} @ ${element.homeTeam.name.default}`,
-        element.gameDate,
-        element.awayTeam.abbrev,
-        element.awayTeam.score || 0,
-        element.homeTeam.abbrev,
-        element.homeTeam.score || 0,
-        (element.gameState === `OFF` && element.awayTeam.score === 0 && element.homeTeam.score === 0) || (element.gameState === `ON`) ? result.INCOMPLETE : 
-        (element.awayTeam.score > element.homeTeam.score ? result.AWAY : result.HOME)
-      )
-      gameList.push(newGame);
-    });
-    //console.log(gameList)
-  });
-
-  const fetchNextDaysData = async () => {
-    try {
-      const response = await axios.get(`https://api-web.nhle.com/v1/score/${tomorrow}`);
-      console.log(response.data);
-    } catch (error) {
-      // Handle error
-      console.error(error);
-    }
-  };
-
-  const fetchYesterdaysData = async () => {
-    try {
-      const response = await axios.get(`https://api-web.nhle.com/v1/score/${yesterday}`);
-      console.log(response.data);
-    } catch (error) {
-      // Handle error
-      console.error(error);
-    }
-  };
-
-  const getTeamSchedule = async () => {
-    try {
-      const response = await axios.get(`https://api-web.nhle.com/v1/club-schedule-season/VAN/20232024`);
-      //console.log(response.data);
-      var teamGameList: Game[] = []
-      response.data.games.forEach((element : any) => {
-        //console.log(element);
-        var newGame = new Game(
-          element.id, 
-          `${element.awayTeam.placeName.default} @ ${element.homeTeam.placeName.default}`,
-          element.gameDate,
-          element.awayTeam.abbrev,
-          element.awayTeam.score || 0,
-          element.homeTeam.abbrev,
-          element.homeTeam.score || 0,
-          (element.gameState === `OFF` && element.awayTeam.score === 0 && element.homeTeam.score === 0) || (element.gameState === `ON`) ? result.INCOMPLETE : 
-          (element.awayTeam.score > element.homeTeam.score ? result.AWAY : result.HOME)
-        )
-        teamGameList.push(newGame);
-      });
-      console.log(teamGameList);
-    } catch (error) {
-      // Handle error
-      console.error(error);
-    }
-  }
-
-  app.get('/tomorrow', fetchNextDaysData);
-  app.get('/yesterday', fetchYesterdaysData);
-  app.get('/teamSchedule', getTeamSchedule);
+app.get('/tomorrow', hockeyHelper.getTomorrowsResults);
+app.get('/yesterday', hockeyHelper.getYesterdaysResults);
+app.get('/today', hockeyHelper.getAllTodaysResults);
 
 server.listen(port, () => {
   console.log(today);
