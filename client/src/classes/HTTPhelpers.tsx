64268954fe532e6b5440520c1d1cf492419b5d07
unknown <bryandang10@gmail.com>
2024-05-19 03:42:52 -0700

    add http helper stubs

---
+++ client/src/classes/HTTPhelpers.tsx
@@ -0,0 +1,15 @@
+// To be used in the dashboard, gets a list of all 
+// the pools that the user is in
+export async function getAllEnteredPools() {
+
+}
+
+// Retrieves a Pool from a code
+export async function getPoolByCode() {
+
+}
+
+// Used to create a Pool
+export function postPool() {
+
+}
