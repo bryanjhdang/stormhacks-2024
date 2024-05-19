62bc61dc1988552d826b466158255c5b0f7b5211
Joshua Li <JoshuaJLi@outlook.com>
2024-05-19 04:24:46 -0700

    Guess endpoint finished.

--- server/src/services/guess.service.ts
+++ server/src/services/guess.service.ts
@@ -1,5 +1,16 @@
-export class GuessService {
+import { firestoreHelper } from "../helpers/firestore.helper";
+import { Guess } from "../models/Guess";
 
+export class GuessService {
+    async giveGuess(userId: string, poolId: string, guesses : Guess[]) {
+        let pool = await firestoreHelper.getPool(poolId);
+        guesses.forEach(guess => {
+            guess.userId = userId;
+            pool.guesses.push(guess);
+        })
+
+        return firestoreHelper.updatePool(pool);
+    }
 }
 
 const guessService = new GuessService();
